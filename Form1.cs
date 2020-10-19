7837271640b35067ff1c1f85542a2ecf96b13e78
Vlad Tsoi <14yozdw@gmail.com>
2020-10-19 19:00:00 +0300

    Исправлены ошибки.

--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,15 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.IO;
-using System.Net;
 using System.Windows.Forms;
 
 namespace Zadanie
 {
     public partial class Form1 : Form
     {
-        SqlConnection conn = MSSQLServerUtils.GetDBConnection();
-
+        string connString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+        
         public Form1()
         {
             InitializeComponent();
@@ -20,20 +18,21 @@ namespace Zadanie
 
         public void DataBase()
         {
+            SqlConnection conn = new SqlConnection(connString);
             dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView1.AllowUserToAddRows = false;
-            conn.Open();
-            SqlDataAdapter adapter = new SqlDataAdapter("SELECT c.Id, c.Name AS 'Название', c.Code AS 'Код страны', " +
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT c.Id, c.Name AS 'Название', c.Code AS 'Код страны', " +
                 "ct.Name AS 'Столица', r.Name AS 'Регион', c.Area AS 'Площадь(кв.км)', c.Population AS 'Население' FROM Country c" +
-                " JOIN Region r ON r.Id = c.RegionId JOIN City ct ON ct.Id = c.CityId", conn);
-            DataSet ds = new DataSet();
-            adapter.Fill(ds);
-            dataGridView1.DataSource = ds.Tables[0];
-            dataGridView1.ReadOnly = true;
-            conn.Close();
+                " JOIN Region r ON r.Id = c.RegionId JOIN City ct ON ct.Id = c.CityId", conn))
+            {
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+                dataGridView1.ReadOnly = true;
+            }
         }
 
-        private async void search_Click(object sender, EventArgs e)
+        private void search_Click(object sender, EventArgs e)
         {
             if (textBoxSearch.Text.Length <= 0)
             {
@@ -43,27 +42,8 @@ namespace Zadanie
             {
                 try
                 {
-                    string countrystring = "https://restcountries.eu/rest/v2/name/" + textBoxSearch.Text;
-                    WebRequest request = WebRequest.Create(countrystring);
-                    request.Method = "GET";
-
-                    request.ContentType = "application/json";
-
-                    Country country;
-                    WebResponse response = await request.GetResponseAsync();
-
-                    string str = string.Empty;
-
-                    using (Stream s = response.GetResponseStream())
-                    {
-                        using (StreamReader read = new StreamReader(s))
-                        {
-                            str = await read.ReadToEndAsync();
-                        }
-                    }
-                    response.Close();
-
-                    country = JsonConvert.DeserializeObject<Country>(str.Substring(1, str.Length - 2));
+                    Search search = new Search { textSearch = textBoxSearch.Text };
+                    Country country = search.GetSearch();
                     label_area.Text = country.area.ToString() + " кв.км";
                     label_cap.Text = country.capital;
                     label_code.Text = country.numericCode.ToString();
@@ -71,93 +51,19 @@ namespace Zadanie
                     label_pop.Text = country.population.ToString() + " чел.";
                     label_reg.Text = country.region;
                     DialogResult vibor = MessageBox.Show("Хотите сохранить информацию в базу данных?", "Да или нет", MessageBoxButtons.YesNo);
+
                     if (vibor == DialogResult.Yes)
-                    {  
-                        
+                    {
                         try
                         {
-                            conn.Open();
-                            SqlDataAdapter adreg = new SqlDataAdapter("SELECT Name FROM Region WHERE Name = '" + label_reg.Text + "'", conn);
-                            SqlDataAdapter adcity = new SqlDataAdapter("SELECT Name FROM City WHERE Name = '" + label_cap.Text + "'", conn);
-                            SqlDataAdapter adcountry = new SqlDataAdapter("SELECT Name FROM Country WHERE Code = '" + label_code.Text + "'", conn);
-                            DataTable dtreg = new DataTable();
-                            DataTable dtcity = new DataTable();
-                            DataTable dtcountry = new DataTable();
-                            adreg.Fill(dtreg);
-                            adcity.Fill(dtcity);
-                            adcountry.Fill(dtcountry);
-                            int resultreg = dtreg.Rows.Count;
-                            int resultcity = dtcity.Rows.Count;
-                            int resultcountry = dtcountry.Rows.Count;
-                            if (resultreg == 0)
-                            {
-                                using (SqlCommand cmd = new SqlCommand("INSERT INTO Region (Name) VALUES(@Name)", conn))
-                                {
-                                    cmd.Parameters.AddWithValue("Name", label_reg.Text);
-                                    int result = cmd.ExecuteNonQuery();
-                                    if (result > 0)
-                                    {
-                                        MessageBox.Show("Данные в таблице Region сохранены.");
-                                    }
-                                }
-                                
-                            }
-
-                            if (resultcity == 0)
-                            {
-                                using (SqlCommand cmd = new SqlCommand("INSERT INTO City (Name) VALUES(@Name)", conn))
-                                {
-                                    cmd.Parameters.AddWithValue("Name", label_cap.Text);
-                                    int result = cmd.ExecuteNonQuery();
-                                    if (result > 0)
-                                    {
-                                        MessageBox.Show("Данные в таблице City сохранены.");
-                                    }
-                                }
-                            }
-                            
-                            if (resultcountry == 0)
-                            {
-                                using (SqlCommand cmd = new SqlCommand("INSERT INTO Country (Name, Code, Area, Population, CityId, RegionId) " +
-                                    "VALUES(@Name, @Code, @Area, @Population, (SELECT Id FROM City WHERE Name = '" + label_cap.Text + "'), " +
-                                    "(SELECT Id FROM Region WHERE Name = '" + label_reg.Text + "'))", conn))
-                                {
-                                    cmd.Parameters.AddWithValue("Name", label_country.Text);
-                                    cmd.Parameters.AddWithValue("Code", label_code.Text);
-                                    cmd.Parameters.AddWithValue("Area", country.area.ToString());
-                                    cmd.Parameters.AddWithValue("Population", country.population.ToString());
-                                    int result = cmd.ExecuteNonQuery();
-                                    if (result > 0)
-                                    {
-                                        MessageBox.Show("Данные в таблице Country сохранены.");
-                                    }
-
-                                }
-                            }
-                            else
-                            {
-                                using (SqlCommand cmd = new SqlCommand("UPDATE c SET c.Name = '" + label_country.Text + "', c.Area = '" 
-                                    + country.area.ToString() + "', c.Population = '" + country.population.ToString() + "', c.CityId = ct.Id, c.RegionId = r.Id" + 
-                                    " FROM Country c, Region r, City ct WHERE c.Code = '" + label_code.Text + "'", conn))
-                                {
-                                    cmd.Parameters.AddWithValue("Name", label_country.Text);
-                                    cmd.Parameters.AddWithValue("Area", country.area.ToString());
-                                    cmd.Parameters.AddWithValue("Population", country.population.ToString());
-                                    int result = cmd.ExecuteNonQuery();
-                                    if (result > 0)
-                                    {
-                                        MessageBox.Show("Данные обновлены!");
-                                    }
-                                }
-                            }
+                            SaveData save = new SaveData { _country = search.GetSearch() };
+                            save.GetSaveCity();
+                            save.GetSaveRegion();
+                            save.GetSaveCountry();
                         }
-                        catch (Exception ex)
+                        catch(Exception ex)
                         {
                             MessageBox.Show("Ошибка: " + ex.Message);
-                        }
-                        finally
-                        {
-                            conn.Close();
                         }
                     }
                 }
@@ -166,6 +72,11 @@ namespace Zadanie
                     MessageBox.Show("Страна " + textBoxSearch.Text + " не найдена");
                 }
             }
+        }
+
+        private void button_refresh_Click(object sender, EventArgs e)
+        {
+            DataBase();
         }
     }
 }
---
+++ SaveData.cs
@@ -0,0 +1,104 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Zadanie
+{
+    class SaveData
+    {
+        string connString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+        public Country _country;
+        SqlConnection conn;
+        public void GetSaveCity()
+        {
+            conn = new SqlConnection(connString);
+            conn.Open();
+            using (SqlDataAdapter adcity = new SqlDataAdapter("SELECT Name FROM City WHERE Name = '" + _country.capital + "'", conn))
+            {
+                DataTable dtcity = new DataTable();
+                adcity.Fill(dtcity);
+                if (dtcity.Rows.Count == 0)
+                {
+                    using (SqlCommand cmd = new SqlCommand("INSERT INTO City (Name) VALUES(@Name)", conn))
+                    {
+                        cmd.Parameters.AddWithValue("Name", _country.capital);
+                        int result = cmd.ExecuteNonQuery();
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Данные в таблице City сохранены.");
+                        }
+                    }
+                }
+            }
+        }
+
+        public void GetSaveRegion()
+        {
+            conn = new SqlConnection(connString);
+            conn.Open();
+            using (SqlDataAdapter adreg = new SqlDataAdapter("SELECT Name FROM Region WHERE Name = '" + _country.region + "'", conn))
+            {
+                DataTable dtreg = new DataTable();
+                adreg.Fill(dtreg);
+                if (dtreg.Rows.Count == 0)
+                {
+                    using (SqlCommand cmd = new SqlCommand("INSERT INTO Region (Name) VALUES(@Name)", conn))
+                    {
+                        cmd.Parameters.AddWithValue("Name", _country.region);
+                        int result = cmd.ExecuteNonQuery();
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Данные в таблице Region сохранены.");
+                        }
+                    }
+                }
+            }
+
+        }
+        public void GetSaveCountry()
+        {
+            conn = new SqlConnection(connString);
+            conn.Open();
+            using (SqlDataAdapter adcountry = new SqlDataAdapter("SELECT Name FROM Country WHERE Code = '" + _country.numericCode + "'", conn))
+            {
+                DataTable dtcountry = new DataTable();
+                adcountry.Fill(dtcountry);
+                if (dtcountry.Rows.Count == 0)
+                {
+                    using (SqlCommand cmd = new SqlCommand("INSERT INTO Country (Name, Code, Area, Population, CityId, RegionId) " +
+                        "VALUES(@Name, @Code, @Area, @Population, (SELECT Id FROM City WHERE Name = '" + _country.capital + "'), " +
+                        "(SELECT Id FROM Region WHERE Name = '" + _country.region + "'))", conn))
+                    {
+                        cmd.Parameters.AddWithValue("Name", _country.name);
+                        cmd.Parameters.AddWithValue("Code", _country.numericCode);
+                        cmd.Parameters.AddWithValue("Area", _country.area.ToString());
+                        cmd.Parameters.AddWithValue("Population", _country.population.ToString());
+                        int result = cmd.ExecuteNonQuery();
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Данные в таблице Country сохранены.");
+                        }
+
+                    }
+                }
+                else
+                {
+                    using (SqlCommand cmd = new SqlCommand("UPDATE c SET c.Name = '" + _country.name + "', c.Area = '"
+                        + _country.area.ToString() + "', c.Population = '" + _country.population.ToString() + "', c.CityId = ct.Id, c.RegionId = r.Id" +
+                        " FROM Country c, Region r, City ct WHERE c.Code = '" + _country.numericCode + "'", conn))
+                    {
+                        cmd.Parameters.AddWithValue("Name", _country.name);
+                        cmd.Parameters.AddWithValue("Area", _country.area.ToString());
+                        cmd.Parameters.AddWithValue("Population", _country.population.ToString());
+                        int result = cmd.ExecuteNonQuery();
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Данные обновлены!");
+                        }
+                    }
+                }
+            }    
+        }
+    }
+}
